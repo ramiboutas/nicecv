2123026c3b2478b5ddf8345fe52e422f79f01bb3
ramiboutas <ramiboutas@hotmail.com>
2021-11-01 09:42:10 +0100

    settings

---
+++ utils/context_processors.py
@@ -0,0 +1,13 @@
+# from tex.models import TexTemplate
+from django.conf import settings
+
+
+# These template context variables will be available in the whole app 
+def nicecv(request):
+    return {
+        # 'tex_templates': TexTemplate.objects.filter(is_active=True), # for tex_templates
+        'site_name': settings.SITE_NAME,
+        'meta_keywords': settings.META_KEYWORDS,
+        'meta_description': settings.META_DESCRIPTION,
+        'request': request
+    }
