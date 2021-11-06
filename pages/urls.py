5f4e595be7f47f5a9592eb2dc4828c37233a4d57
ramiboutas <ramiboutas@hotmail.com>
2021-11-06 20:57:14 +0100

    allauth 2

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, PrivacyPolicyView, TermsAndConditionsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
+    path('terms-conditions/', TermsAndConditionsView.as_view(), name='terms-conditions'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+
+class PrivacyPolicyView(TemplateView):
+    template_name = 'pages/privacy-policy.html'
+
+
+
+class TermsAndConditionsView(TemplateView):
+    template_name = 'pages/terms-conditions.html'
