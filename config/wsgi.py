321f24f29c84306bfea1db261bbc4bac83fe3188
ramiboutas <ramiboutas@hotmail.com>
2021-11-08 08:16:18 +0100

    wsgi dotenv

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
-
+dotenv.read_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
