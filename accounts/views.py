c96c6e16343a6e0ee429d29778a5313a878515a5
ramiboutas <ramiboutas@hotmail.com>
2021-11-20 03:31:15 +0100

    home and local

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
 from django.views.decorators.http import require_http_methods
 from  django.shortcuts import redirect, render
+from django.contrib import messages
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from .models import EarlyAdopter
 
@@ -12,11 +16,14 @@ class MyAccountView(TemplateView):
 @require_http_methods(["POST"])
 def early_adopters_view(request):
     email = request.POST.get('email')
-    print(email)
     try:
+        validate_email(email)
         EarlyAdopter.objects.create(email=email)
         return render(request, 'early_adopters_thanks.html')
-    except:
+    except ValidationError as e:
+        messages.error(request, _('Enter a valid email'))
+        return redirect('home')
+    else:
         return redirect('home')
 
 
