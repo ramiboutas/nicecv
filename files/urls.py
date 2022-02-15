6bd0fbbf2a82abed1d5de7e5c6329af6ce262576
ramiboutas <ramiboutas@hotmail.com>
2022-02-15 22:39:32 +0100

    task_id and new templates in fiels app

--- files/urls.py
+++ files/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import resumefile_list_view
+from .views import start_creating_resume_files_view, resume_file_list_view
 
 
 urlpatterns = [
-    path('<uuid:pk>/resumes', resumefile_list_view, name='files_resume_file_list')
+    path('start-creating-resumes/<uuid:pk>', start_creating_resume_files_view, name='files_resume_file_list'),
+    path('resumes/<uuid:pk>', resume_file_list_view, name='files_resume_file_list'),
     # path('pdf/<int:pk>/', download_pdf_view, name='files_download_pdf'),
     # path('download-coverletter/<int:pk>/', download_coverletter_view, name='files_download_coverletter'),
 ]
--- files/views.py
+++ files/views.py
@@ -4,8 +4,13 @@ from django.views.generic.list import ListView
 from .models import ResumeFile
 from .tasks import create_resume_file_objects
 
-def resumefile_list_view(request, pk):
-    # create_resume_file_objects.delay(pk=pk)
+def start_creating_resume_files_view(request, pk):
+    result =  create_resume_file_objects.delay(pk=pk)
+    context = {'task_id': result.id}
+    return render(request, 'files/start_creating_resume_files.html', context)
+
+
+def resume_file_list_view(request, pk):
     qs = ResumeFile.objects.filter(profile__user=request.user, profile__pk=pk)
     context = {'object_list': qs}
     return render(request, 'files/resumefile_list.html', context)
