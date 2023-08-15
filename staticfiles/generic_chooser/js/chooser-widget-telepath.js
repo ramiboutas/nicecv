5180c7ee85af4b5b4ade51fa7c6d2efc6a26f1e5
ramiboutas <ramiboutas@hotmail.com>
2023-08-15 15:39:54 +0200

    staticfiles removed

--- staticfiles/generic_chooser/js/chooser-widget-telepath.js
+++
@@ -1,3 +0,0 @@
-(function() {
-    window.telepath.register('wagtail_generic_chooser.widgets.Chooser', window.WagtailGenericChooserWidgetFactory);
-})();
--- staticfiles/generic_chooser/js/linked-field-chooser-widget-telepath.js
+++
@@ -1,3 +0,0 @@
-(function() {
-    window.telepath.register('wagtail_generic_chooser.widgets.LinkedFieldChooser', window.LinkedFieldChooserWidgetFactory);
-})();
