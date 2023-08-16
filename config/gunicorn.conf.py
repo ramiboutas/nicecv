19179b05cdfdd3f246f82f8e8f83e64f67620925
ramiboutas <ramiboutas@hotmail.com>
2023-08-16 10:23:55 +0200

    gunicorn config file

---
+++ config/gunicorn.conf.py
@@ -0,0 +1,10 @@
+# https://docs.gunicorn.org/en/stable/settings.html#settings
+
+import multiprocessing
+
+max_requests = 1000
+max_requests_jitter = 50
+
+log_file = "-"
+
+workers = multiprocessing.cpu_count() * 2 + 1
