84f4e91eeae4510924a83ba4ad4ec7975e610702
ramiboutas <ramiboutas@hotmail.com>
2023-08-16 11:06:44 +0200

    now celery module is there

---
+++ config/celery_app.py
@@ -0,0 +1,16 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+app = Celery("config")
+app.config_from_object(settings, namespace="CELERY")
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
