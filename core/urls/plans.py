a82721213b134908cdc095e1881e7900e7b92328
Rami <ramiboutas@hotmail.com>
2024-01-01 22:06:56 +0100

    import issue

--- core/urls/plans.py
+++ core/urls/plans.py
@@ -5,7 +5,6 @@ from ..views.plans import payment_fail
 from ..views.plans import payment_success
 from ..views.plans import plan_detail
 from ..views.plans import plan_list
-from ..views.plans import stripe_webhook
 
 
 urlpatterns = [
@@ -15,5 +14,4 @@ urlpatterns = [
     path("checkout/<int:id>/", checkout, name="plan_checkout"),
     path("payment-successed/", payment_success, name="payment_successed"),
     path("payment-failed/", payment_fail, name="payment_failed"),
-    path("webhook/", stripe_webhook, name="webhook_intent"),
 ]
